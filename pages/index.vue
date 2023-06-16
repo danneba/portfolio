dbd9698e930df89e4c5beec4018d9f2e65c2e60e
danneba <danielkassahun2@gmail.com>
2023-06-16 16:38:12 +0300

    done

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+  
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+
+</script>
+<template>
+    <div>
+        <p>CMS</p>
+    </div>
+</template>
